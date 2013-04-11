eb5b9a956747e2f7ec821c75a2f4972bb307fed4
Furqankhanzada <furqan.khanzada@gmail.com>
2013-04-12 04:54:40 +0500

    'init'

---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== Wp Twitter Feeds ===
+Contributors: application bakers
+Donate link: http://www.appbakerz.com/wp-plugins/wp-twitter-feeds/donate/
+Tags: twitter, tweets, twitter feeds, infinite twitter feeds, infinile, ajax, backbonejs, multilingual
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Just another Twitter Feeds plugin. Simple but flexible.
+
+== Description ==
+
---
+++ wp-twitter-feeds.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Wp Twitter Feeds
+Plugin URI: http://www.appbakerz.com/
+Description: Just another Twitter Feeds plugin. Simple but flexible.
+Author: Application Bakers
+Author URI: http://www.appbakerz.com
+Text Domain: wp-twitter-feeds
+Domain Path: /languages/
+Version: 0.1
+*/
+
+/*  Copyright 2013 Application Bakers (email: info@appbakerz.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/

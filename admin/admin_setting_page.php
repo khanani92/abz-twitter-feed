f6d864445e2907e7936414baa48ffdce41c0ea79
Furqankhanzada <#>
2013-04-20 00:43:12 +0500

    added shortcode and some fileds on setting page

--- admin/admin_setting_page.php
+++ admin/admin_setting_page.php
@@ -27,18 +27,22 @@ function my_admin_init() {
 	add_settings_section( 'general_settings_section', __('General Settings', 'tfs'), 'general_settings_callback', 'twitter-feeds' );
 
 	// Fields
-	add_settings_field( 'field_1_callback', __('Tfs fiels 1', 'tfs'), 'field_1_callback', 'twitter-feeds', 'required_settings_section' );
-	add_settings_field( 'field_2_callback', __('Tfs fiels 2', 'tfs'), 'field_2_callback', 'twitter-feeds', 'required_settings_section' );
-	add_settings_field( 'field_3_callback', __('Tfs fiels 3', 'tfs'), 'field_3_callback', 'twitter-feeds', 'required_settings_section' );
-	add_settings_field( 'field_4_callback', __('Tfs fiels 4', 'tfs'), 'field_4_callback', 'twitter-feeds', 'required_settings_section' );
+	add_settings_field( 'field_1_callback', __('Consumer Key', 'tfs'), 'field_1_callback', 'twitter-feeds', 'required_settings_section' );
+	add_settings_field( 'field_2_callback', __('Consumer Secret', 'tfs'), 'field_2_callback', 'twitter-feeds', 'required_settings_section' );
+	add_settings_field( 'field_3_callback', __('Access Token', 'tfs'), 'field_3_callback', 'twitter-feeds', 'required_settings_section' );
+	add_settings_field( 'field_4_callback', __('Access Token Secret', 'tfs'), 'field_4_callback', 'twitter-feeds', 'required_settings_section' );
 	add_settings_field( 'field_5_callback', __('Tfs fiels 5', 'tfs'), 'field_5_callback', 'twitter-feeds', 'general_settings_section' );
 	add_settings_field( 'field_6_callback', __('Tfs fiels 6', 'tfs'), 'field_6_callback', 'twitter-feeds', 'general_settings_section' );
 }
 $settings = get_option( 'tfs-settings' );
  
-function required_settings_callback() {
-	printf(__('Copy and paste your <strong>Keys</strong> and <strong>Tokens</strong>.', 'tfs'));
-}
+function required_settings_callback() { ?>
+	<ol>
+		<li><?php printf(__("Register this site as an application on <strong><a target='_blank' href='http://dev.twitter.com/apps/new'>Twitter's application registration page</a></strong>.", "tfs")); ?></li>	        
+		<li><?php printf(__("Copy and paste your <strong>Key,Token</strong> and <strong>Secrets</strong>.", "tfs")); ?></li>
+	</ol>
+	
+<?php }
 
 function general_settings_callback() {
 	printf(__('Genrel Settings Area.', 'tfs'));
--- admin/tfs_shortcodes.php
+++ admin/tfs_shortcodes.php
@@ -1,1 +1,11 @@
 <?php
+// [twitter-feeds foo="foo-value"]
+function tfs_shortcode( $atts ) {
+	extract( shortcode_atts( array(
+		'foo' => 'something',
+		'bar' => 'something else',
+	), $atts ) );
+
+	return "foo = {$foo}";
+}
+add_shortcode( 'twitter-feeds', 'tfs_shortcode' );

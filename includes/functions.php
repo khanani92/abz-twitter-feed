da29bde0d990bb793ab288881e1f07c6342c9a0c
Furqankhanzada <furqan.khanzada@gmail.com>
2013-04-29 20:46:22 +0500

    ajax handler updated

--- includes/functions.php
+++ includes/functions.php
@@ -1,4 +1,7 @@
 <?php
+require_once ABZ_TWITTER_FEED_PLUGIN_DIR . '/lib/twitter_api_client.php';
+require_once ABZ_TWITTER_FEED_PLUGIN_DIR . '/lib/API_cache.php';
+require_once ABZ_TWITTER_FEED_PLUGIN_DIR . '/lib/cached_twitter_timeline_reader.php';
 //////////////////////////////////////////////////////////////////////////////
 // Adding css and js files on theme
 //////////////////////////////////////////////////////////////////////////////
@@ -21,17 +24,17 @@ function abz_get_twitter_feed(){
 	//$title = $_POST['title'];
 
 	//TODO: Fill these values by the ones supplied on extension's settings page
-	$consumer_key = '';
-	$consumer_secret = '';
+	$consumer_key = 'FyMvI7VdIHa77M5YlodA';
+	$consumer_secret = '5VXaEoWUQAC7Z3MkiA3fAKy0gqi1hnSoXkSUNctLA';
 
-	$oauth_access_token = '';
-	$oauth_access_token_secret = '';
+	$oauth_access_token = '526178478-HaGAiDnLuPOGLvqSkqXoxE6I9IICUqK2TQ8QibUf';
+	$oauth_access_token_secret = 'wdLkH5TiDsXLjNcw7ILD8mU5gdK3j6IUzdN53onfA';
 
 	//TODO: If values missing, send an error and if admin_logged_in send actual reason 
 
 	$oauth = array( 'oauth_consumer_key' => $consumer_key,
-					'oauth_token' => $oauth_access_token,
 					'oauth_consumer_secret' => $oauth_consumer_secret,
+					'oauth_token' => $oauth_access_token,
 					'oauth_access_token_secret' => $oauth_access_token_secret
 					);
 
@@ -43,5 +46,5 @@ function abz_get_twitter_feed(){
 	die($results);
 }
 // creating Ajax call for WordPress
-add_action( 'wp_ajax_nopriv_gettwitterfeed', 'abz_get_twitter_feed' );
-add_action( 'wp_ajax_gettwitterfeed', 'abz_get_twitter_feed' );
+add_action( 'wp_ajax_nopriv_abz_get_twitter_feed', 'abz_get_twitter_feed' );
+add_action( 'wp_ajax_abz_get_twitter_feed', 'abz_get_twitter_feed' );
